78e264f69e73d034e4a44d588bbb5972554e0fcd
Manasvi Agarwal <44316882+mnsv73@users.noreply.github.com>
2020-07-06 11:48:04 +0530

    Add files via upload

--- gtrends.py
+++ gtrends.py
@@ -1,25 +1,32 @@
 from pytrends.request import TrendReq
 import csv
 
-pytrends = TrendReq(hl='en-US', tz=360)
+choice = input("Enter 1 for top 5 words and 2 for complete list: ")
+print(type(choice))
 
-with open('relatedlist.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    i = 5
-    for row in csv_reader:
-        if (i<1): break
-        pytrends.build_payload(row, cat=0, timeframe='today 5-y', geo= '', gprop='')
-        df = pytrends.interest_over_time()
-        x = (df[row].sum())
-        if (x[0]>2000 and x[0]< 20000):
-            print(x)
-        i=i-1
+if (choice == '1'):
+    pytrends = TrendReq(hl='en-US', tz=360)
+    with open('relatedlist.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        i = 5
+        for row in csv_reader:
+            if (i<1): break
+            pytrends.build_payload(row, cat=0, timeframe='today 5-y', geo= '', gprop='')
+            df = pytrends.interest_over_time()
+            x = (df[row].sum())
+            if (x[0]>2000 and x[0]< 20000):
+                print(x)
+            i=i-1
 
 #get full list
-with open('relatedlist.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        print(row)
+elif(choice == '2'):
+    with open('relatedlist.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            print(row)
+
+else:
+    print("invalid choice")
 
             
 
